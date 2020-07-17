7654077f43dfc4f3168a3ece452228b1b8e35075
mwinitch <michaelwin26@gmail.com>
2020-07-16 20:41:58 -0400

    Removing old comments from server.js

--- backend/server.js
+++ backend/server.js
@@ -12,16 +12,6 @@ app.use(express.json());
 
 // Connect to the MongoDB
 const uri = process.env.ATLAS_URI;
-/*
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
-const connection = mongoose.connection;
-
-
-connection.once('open', () => {
-    console.log("Connected to MongoDB")
-});
-*/
-// useCreateIndex: true
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.log(err));
